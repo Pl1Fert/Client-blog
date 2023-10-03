4531744a9999118fc1d989238dfc3ea7d68eb6f2
Alexey Rusel <alexeyrusel@gmail.com>
2023-10-03 17:00:49 +0300

    feat: add translations

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,7 +3,6 @@ export * from "./appRoutes";
 export * from "./socials";
 export * from "./authors";
 export * from "./links";
-export * from "./privacyPolicy";
 export * from "./envVariables";
 export * from "./citiesCoordinates";
 export * from "./mailQueries";
