9fe62d5ae79c3483efe5ed83bb0f90da14094fcb
Alexey Rusel <alexeyrusel@gmail.com>
2023-10-04 13:46:41 +0300

    feat: add modal

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -1,22 +1,16 @@
 import { FC } from "react";
 
-import { useTranslation } from "@/app/i18n";
 import { NavBar } from "@/components";
-import { Button } from "@/UI";
+
+import Modal from "../Modal/modal";
 
 import { HeaderProps } from "./header.interfaces";
 
 import styles from "./header.module.scss";
 
-export const Header: FC<HeaderProps> = async ({ lng }) => {
-    const { t } = await useTranslation(lng, "header");
-
-    return (
-        <header className={styles.header}>
-            <NavBar lng={lng} />
-            <div>
-                <Button white>{t("button")}</Button>
-            </div>
-        </header>
-    );
-};
+export const Header: FC<HeaderProps> = ({ lng }) => (
+    <header className={styles.header}>
+        <NavBar lng={lng} />
+        <Modal lng={lng} />
+    </header>
+);
---
+++ src/components/Modal/modal.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { FC, useState } from "react";
+
+import { useTranslation } from "@/app/i18n/client";
+import { Button } from "@/UI";
+
+import { ModalProps } from "./modal.interfaces";
+
+import styles from "./modal.module.scss";
+
+const Modal: FC<ModalProps> = ({ lng }) => {
+    const { t } = useTranslation(lng, "header");
+    const [isVisible, setIsVisible] = useState<boolean>(false);
+
+    const toggleVisible = (): void => {
+        setIsVisible((prev) => !prev);
+    };
+
+    const modalContent = (
+        <div onClick={toggleVisible} className={styles.modalWrapper}>
+            <div className={styles.modalContent}>
+                <iframe
+                    src="https://www.youtube.com/embed/dQw4w9WgXcQ?si=0rizBOc6_jxykHs3"
+                    title="YouTube video player"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    allowFullScreen
+                    className={styles.modalVideo}
+                />
+            </div>
+            <div className={styles.modalBackground} />
+        </div>
+    );
+
+    return (
+        <>
+            <div>
+                <Button white onClick={toggleVisible}>
+                    {t("button")}
+                </Button>
+            </div>
+            {isVisible && modalContent}
+        </>
+    );
+};
+
+export default Modal;
