5a35d745558c251a2aad3173afe66e29b3409474
Alexey Rusel <alexeyrusel@gmail.com>
2023-10-05 17:36:26 +0300

    Dev (#9)
    * project setup
    
    * feat: add pages
    
    * feat: add style base
    
    * feat: add assets
    
    * feat: add basic layout
    
    * feat: add button ui component
    
    * feat: add loading ui
    
    * feat: update ui library
    
    * feat: add basic layout
    
    * feat: add JoinUs component
    
    * feat: add privacy policy page
    
    * feat: add utility pages
    
    * feat: add author cards
    
    * feat: add blog page
    
    * feat: add map with coordinates
    
    * feat: add contact page layout
    
    * feat: update ui library
    
    * feat: add contact form with emailjs
    
    * Delete .env
    
    * feat: add form validation
    
    * feat: add carousel
    
    * feat: add category cards
    
    * feat: finish home page
    
    * feat: add i18n structure
    
    * feat: add components translation
    
    * feat: add translations
    
    * feat: add pages translations
    
    * feat: add posts
    
    * feat: add author page layout
    
    * feat: add post page
    
    * feat: add posts page
    
    * feat: add modal
    
    * feat: add category page
    
    * feat: add adaptive layout
    
    * fix: translation
    
    * feat: add tests configs
    
    * feat: add tests
    
    * feat: add ci
    
    * feat: add animate on scroll
    
    * docs: update readme
    
    * fix: build errors

---
+++ src/app/global-error.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { FC, useEffect } from "react";
+
+import { Button } from "@/UI";
+
+import styles from "./[lng]/error.module.scss";
+
+interface ErrorProps {
+    error: Error & { digest?: string };
+    reset: () => void;
+}
+
+const Error: FC<ErrorProps> = ({ error, reset }) => {
+    useEffect(() => {
+        console.error(error);
+    }, [error]);
+
+    const clickHandler = (): void => {
+        reset();
+    };
+
+    return (
+        <html lang="en">
+            <body>
+                <section className={styles.section}>
+                    <h2 className={styles.title}>Something went wrong!</h2>
+                    <Button yellow width="50%" onClick={clickHandler}>
+                        Try Again
+                    </Button>
+                </section>
+            </body>
+        </html>
+    );
+};
+
+export default Error;
